2d943f4bf8d0d2400eb4a7f10e515e6c1242979b
ACAPP-dev <acapp909@gmail.com>
2020-10-15 15:39:08 -0700

    begin navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './containers/NavBarContainer'
+import NavBarContainer from './containers/NavBarContainer';
 
 function App() {
   return (
     <div className="App">
+      <navbar>
+        < NavBarContainer / >
+
+      </navbar>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Andrewflix Practice
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
---
+++ src/containers/NavBarContainer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from '../images/logo.png'
+
+export default class NavBar extends React.Component {
+    render() {
+        return (
+            <div id='navbar-div'>
+                <div>
+                    <img id='andrew-logo' src={logo} />
+                </div>
+                <div className='navbar-menu-div'>
+                    <span style={{"font-weight":'700'}}>Home</span>
+                    <span>TV Shows</span>
+                    <span>Movies</span>
+                    <span>Latest</span>
+                    <span>My List</span>
+                </div>
+                <div className='navbar-menu-div'>
+                    <span>Search Img</span>
+                    <span>KIDS</span>
+                    <span>DVD</span>
+                    <span>Gift Img</span>
+                    <span>Bell Img</span>
+                    <span>Login Img</span>
+                </div>
+            </div>
+
+        )
+    }
+
+}
+
